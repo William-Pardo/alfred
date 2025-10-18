10575ee154352b074c7e9842bf70b3e9591deeee
Tu Nombre de Usuario de GitHub <gengepardo@gmail.com>
2025-10-18 00:19:44 -0500

    test: smoke que verifica titulo y lang; package estable

---
+++ tests/smoke.js
@@ -0,0 +1,20 @@
+﻿import fs from "node:fs";
+
+function fail(msg) { console.error(msg); process.exit(1); }
+
+let html;
+try {
+  html = fs.readFileSync("index.html", "utf8");
+} catch {
+  fail("No se encontró index.html en la raíz");
+}
+
+if (!/<title>\s*Hola\s+Alfred\s*<\/title>/i.test(html)) {
+  fail("El título 'Hola Alfred' no está en <title>");
+}
+
+if (!/<html[^>]*\blang=["\']es["\']/i.test(html)) {
+  fail('Falta lang="es" en la etiqueta <html>');
+}
+
+console.log("Smoke test OK");
