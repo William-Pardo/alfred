7b42d2023d387a24c4537f7b1ec256fe1e2c9580
Tu Nombre de Usuario de GitHub <gengepardo@gmail.com>
2025-10-17 23:50:10 -0500

    [alfred] T2 server.js

--- server.js
+++ server.js
@@ -22,3 +22,11 @@ const server = http.createServer((req, res) => {
       }
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
+    });
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
