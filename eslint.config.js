3d51b3e530aa7e0daff36f31541c0e3bbda48b18
Tu Nombre de Usuario de GitHub <gengepardo@gmail.com>
2025-10-18 00:00:10 -0500

    [alfred] T1 package.json

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+﻿import js from "@eslint/js";
+
+export default [
+  { ignores: ["node_modules/**", "dist/**", "alfred/**"] },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      // Ajusta reglas si molestan durante el bootstrap:
+      "no-unused-vars": "off",
+      "no-undef": "off"
+    }
+  }
+];
